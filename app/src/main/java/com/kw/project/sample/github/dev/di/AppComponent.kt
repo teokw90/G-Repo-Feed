129c23342950505588b037366b86e29693cff627
Kuan Wah Teo <teokw90@gmail.com>
2021-01-18 16:43:21 +0800

    Implement DI into Project

--- app/src/main/java/com/kw/project/sample/github/dev/di/AppComponent.kt
+++ app/src/main/java/com/kw/project/sample/github/dev/di/AppComponent.kt
@@ -1,4 +1,20 @@
 package com.kw.project.sample.github.dev.di
 
-class AppComponent {
+import com.kw.project.module.core.di.CoreComponent
+import com.kw.project.module.core.di.scopes.AppScope
+import com.kw.project.sample.github.dev.MainApplication
+import dagger.Component
+
+@AppScope
+@Component(
+    dependencies = [CoreComponent::class],
+    modules = [AppModule::class]
+)
+interface AppComponent {
+    /**
+     * Inject dependencies on application.
+     *
+     * @param application
+     */
+    fun inject(application: MainApplication)
 }
--- app/src/main/java/com/kw/project/sample/github/dev/di/AppModule.kt
+++ app/src/main/java/com/kw/project/sample/github/dev/di/AppModule.kt
@@ -1,4 +1,24 @@
 package com.kw.project.sample.github.dev.di
 
+import android.content.Context
+import com.kw.project.sample.github.dev.MainApplication
+import dagger.Module
+import dagger.Provides
+
+/**
+ * Class that contributes to the object graph [AppComponent].
+ *
+ * @see Module
+ */
+@Module
 class AppModule {
+    /**
+     * Create a provider method binding for [Context].
+     *
+     * @param application
+     * @return Instance of context
+     * @see Provides
+     */
+    @Provides
+    fun provideContext(application: MainApplication): Context = application.applicationContext
 }
